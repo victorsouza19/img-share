765bc6d6d30af3d068143876ad8f231873f4b493
victorsouza19 <victor.cristofalo@hotmail.com>
2021-12-19 21:18:40 -0300

    wip: structure and user register - update folder structure to services and models - new route POST /users to register a user

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express'),
-app = express();
-let mongoose = require("mongoose");
+app = express(),
+mongoose = require("mongoose"),
+UserService = require("./services/UserService"),
+bcrypt = require('bcrypt');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -8,16 +10,53 @@ app.use(express.json());
 mongoose.connect("mongodb://localhost:27017/imgshare", {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(res => {
-  console.log("Database connected.");
-
-}).catch(err => { 
+}).then(() => {}).catch(err => { 
   console.log(err); 
-
 });
 
 app.get("/", (req, res) => {
   res.json({});
+});
+
+app.post("/users", async (req, res) => {
+  const {name, email, password} = req.body;
+
+  if(email == '' || name == '' || password == ''){
+    res.status(400);
+    return res.json({err: 'Invalid fields.'});
+
+  }
+
+  let salt = await bcrypt.genSalt(10);
+  let hash = await bcrypt.hash(password, salt);
+
+  const user = { name, email, password: hash};
+
+  let userExists = await UserService.FindByEmail(email);
+
+  if(userExists.status){
+    if(userExists.result != undefined){
+      res.status(400);
+      return res.json({err: 'E-mail already exists.'});
+      
+    }else{
+      let result = await UserService.Create(user);
+
+      if(result.status){
+        res.status(200);
+        return res.json({email});
+
+      }else{
+        res.status(500);
+        return res.json({err: result.err});
+
+      }
+    }
+
+  }else{
+    res.status(500);
+    return res.json({err: userExists.err});
+  }
 });
 
 module.exports = app;
---
+++ src/services/UserService.js
@@ -0,0 +1,34 @@
+const schema = require("../models/User"), 
+mongoose = require('mongoose');
+
+const User = mongoose.model("Users", schema);
+
+class UserService{
+
+  async Create(user){
+    try {
+      let newUser = new User(user);
+      await newUser.save();
+
+      return {status: true};
+
+    } catch (error) {
+      console.log(error);
+      return {status: false, error};
+    }
+  }
+
+  async FindByEmail(email){
+    try {
+      
+      let result = await User.findOne({email});
+      return {status: true, result};
+
+    } catch (error) {
+      console.log(error);
+      return {status: false, err: error};
+    }
+  }
+}
+
+module.exports = new UserService();
